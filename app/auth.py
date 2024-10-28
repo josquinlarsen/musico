af7f50a788bf73cfaaf05fd2081d3c0fdc944faf
Josquin Larsen <jsqnlrsn@gmail.com>
2024-10-27 21:13:16 -0400

    added wrapper from tuto

--- app/auth.py
+++ app/auth.py
@@ -12,6 +12,19 @@ from app.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+def login_required(view):
+    """
+    wrapper that requires user be logged in to access view
+    """
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
