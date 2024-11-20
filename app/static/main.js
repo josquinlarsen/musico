2b95fad7e680b46b9403a41e9ac9444444787e5a
Josquin Larsen <jsqnlrsn@gmail.com>
2024-11-19 22:26:32 -0500

    removing files

--- app/static/main.js
+++
@@ -1,6 +0,0 @@
-// delete js function
-
-function confirmDelete(event, name) {
-    const confirmed = confirm(`Do you want to delete ${name}? This cannot be undone.`);
-    if (!confirmed) event.preventDefault();
-}
